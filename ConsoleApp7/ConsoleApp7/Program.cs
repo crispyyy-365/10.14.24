41d76c996202d60c6223b83ad431e29b538c2f72
Huseyn Abbasov <huseynaa-bp217@code.edu.az>
2024-10-14 03:56:44 +0400

    initial commit

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student student1 = new("Huseyn", "Abbasov", "TK-99", 66);
+
+            student1.GetInfo();
+            student1.CheckGraduation();
+            student1.GetDiplomDegree();
+
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp7
+{
+    class student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduated;
+
+       public student(string name, string surname, string group, int point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+
+            if (point >= 0 && point < 65)
+            {
+                IsGraduated = false;
+            }
+            else
+            {
+                IsGraduated = true;
+            }
+        }
+        public void GetInfo(string name, string surname, string group, int point)
+        {
+            Console.WriteLine($"Name : {name} , surname : {surname} , group : {group} , point : {point}");
+        }
+        public void CheckGraduation(bool isgraduated)
+        {
+            if (isgraduated)
+            {
+                Console.WriteLine("the student is graduated.");
+            }
+            else
+            {
+                Console.WriteLine("the student is not graduated , because the point is below 65.");
+            }
+        }
+
+        public void GetDiplomDegree(int point)
+        {
+            if (0 <= point && point <= 65)
+            {
+                Console.WriteLine("the student has no diploma.");
+            }
+            else if (65 < point && point <= 80)
+            {
+                Console.WriteLine("the student has an ordinary diploma.");
+            }
+            else if (80 < point && point <= 90)
+            {
+                Console.WriteLine("the student has an honor diploma.");
+            }
+            else if (90 < point && point <= 100)
+            {
+                Console.WriteLine("the student has a high honor diploma . ");
+            }
+            else
+            {
+                Console.WriteLine("there is no such point!");
+            }
+        }
+    }
+}
